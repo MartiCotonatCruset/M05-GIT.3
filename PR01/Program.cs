7d79a49827813a3642c0bf3aabf23abb8cff970b
Adria <a2208022@institutmontilivi.cat>
2023-01-23 17:35:08 +0100

    VersioMenu

--- PR01/Program.cs
+++ PR01/Program.cs
@@ -10,7 +10,41 @@ namespace PR01
     {
         static void Main(string[] args)
         {
-
+            ConsoleKeyInfo tecla;
+            do
+            {
+                Console.Clear();
+                ShowOptions();
+                tecla = Console.ReadKey();
+                Console.Clear();
+                switch (tecla.Key)
+                {
+                    case ConsoleKey.D1:
+                        Topic1();
+                        break;
+                    case ConsoleKey.D2:
+                        Topic2();
+                        break;
+                    case ConsoleKey.D3:
+                        Topic3();
+                        break;
+                    case ConsoleKey.D0:
+                        MsgNextScreen("PRESS ANY KEY 2 EXIT");
+                        break;
+                }
+            }
+            while (tecla.Key != ConsoleKey.D0);
+        }
+        /// <summary>
+        /// This action shows a menu
+        /// </summary>
+        public static void ShowOptions()
+        {
+            Console.Clear();
+            Console.WriteLine("1 - MATHS TOPIC");
+            Console.WriteLine("2 - GEOGRAPHY");
+            Console.WriteLine("3 - ART");
+            Console.WriteLine("0 - PRESS ANY KEY 2 EXIT");
         }
     }
 }
